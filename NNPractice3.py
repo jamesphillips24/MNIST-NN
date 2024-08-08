f47829beecfb7ff3963d04b024cdf3e6562eecdf
James Phillips <166641424+jamesphillips24@users.noreply.github.com>
2024-08-08 18:05:15 -0400

    Add files via upload

--- NNPractice3.py
+++ NNPractice3.py
@@ -3,7 +3,7 @@ import numpy as np
 import random
 import math
 
-training_data = pd.read_csv("/Users/james/Visual-Studio/NN/mnist_train.csv")
+training_data = pd.read_csv('mnist_train.csv')
 training_data = np.array(training_data)
 
 label = np.zeros((60000, 10))
@@ -11,7 +11,7 @@ for i in range(60000):
     label[i][training_data[i][0]] = 1
 training_data = training_data[0:, 1:] / 255 # normalized
 
-test_data = pd.read_csv("/Users/james/Visual-Studio/NN/mnist_test.csv")
+test_data = pd.read_csv('mnist_test.csv')
 test_data = np.array(test_data)
 
 label1 = np.zeros((10000, 10))
@@ -29,16 +29,9 @@ def softmax(z):
     e_x = np.exp(z - np.max(z))
     return e_x/np.sum(e_x)
 
-def ssr(z, labels):
-    return np.sum(np.square(labels - z))
-
 def crossEntropy(z, labels):
     i = np.where(labels == 1)[0]
-    try:
-        return -math.log(z[i] + 1e-8)
-    except:
-        print(z)
-        print(labels)
+    return -math.log(z[i][0] + 1e-8)
 
 def initialize(layer1_size, layer2_size, output_size):
     w1 = np.random.randn(layer1_size, np.shape(training_data[0])[0]) * np.sqrt(2/np.shape(training_data[0])[0]) # 8 x 784
@@ -60,15 +53,15 @@ def forward(dataset):
     return z1, z2, z3, a1, a2, a3
 
 def backward(dataset, y, a1, a2, a3, z1, z2, z3, w1, w2, w3, dw1, db1, dw2, db2, dw3, db3):
-    delta3 = a3 - y # Last layer node deltas
+    delta3 = a3 - y # Last layer node deltas from raw output to cross entropy
     dw3 += np.outer(delta3, a2)
     db3 += delta3
 
-    delta2 = drelu(z2) * np.dot(w3.T, delta3.flatten())
+    delta2 = drelu(z2) * np.dot(w3.T, delta3.flatten()) # Second hidden layer deltas
     dw2 += np.outer(delta2, a1)
     db2 += delta2
 
-    delta1 = drelu(z1) * np.dot(w2.T, delta2.flatten())
+    delta1 = drelu(z1) * np.dot(w2.T, delta2.flatten()) # First hidden layer deltas
     dw1 += np.outer(delta1, dataset)
     db1 += delta1
 
@@ -82,32 +75,36 @@ outputsize = 10
 w1, w2, w3, b1, b2, b3 = initialize(firstlayersize, secondlayersize, outputsize)
 batchsize = 100
 
-for i in range(600):
-    loss = 0
-    dw1 = np.zeros((firstlayersize, 784))
-    db1 = np.zeros(firstlayersize)
-    dw2 = np.zeros((secondlayersize, firstlayersize))
-    db2 = np.zeros(secondlayersize)
-    dw3 = np.zeros((10, secondlayersize))
-    db3 = np.zeros(outputsize)
-    for j in range(batchsize):
-        z1, z2, z3, a1, a2, a3 = forward(training_data[j + batchsize * i])
-        loss += crossEntropy(a3, label[j + batchsize * i])
-        dw1, db1, dw2, db2, dw3, db3 = backward(training_data[j + batchsize * i], label[j + batchsize * i], a1, a2, a3, z1, z2, z3, w1, w2, w3, dw1, db1, dw2, db2, dw3, db3)
-    w1 -= 0.1 * dw1 / batchsize
-    b1 -= 0.1 * db1 / batchsize
-    w2 -= 0.1 * dw2 / batchsize
-    b2 -= 0.1 * db2 / batchsize
-    w3 -= 0.1 * dw3 / batchsize
-    b3 -= 0.1 * db3 / batchsize
-    print(loss)
+# This chunk is soon to be cleaned up and streamlined
+for k in range(3): #If you want to loop over the data multiple times
+    for i in range(int(np.shape(training_data)[0] / batchsize)): # total samples / batchsize
+        loss = 0
+        dw1 = np.zeros((firstlayersize, 784))
+        db1 = np.zeros(firstlayersize)
+        dw2 = np.zeros((secondlayersize, firstlayersize))
+        db2 = np.zeros(secondlayersize)
+        dw3 = np.zeros((10, secondlayersize))
+        db3 = np.zeros(outputsize)
+        for j in range(batchsize):
+            z1, z2, z3, a1, a2, a3 = forward(training_data[j + batchsize * i])
+            loss += crossEntropy(a3, label[j + batchsize * i])
+            dw1, db1, dw2, db2, dw3, db3 = backward(training_data[j + batchsize * i], label[j + batchsize * i], a1, a2, a3, z1, z2, z3, w1, w2, w3, dw1, db1, dw2, db2, dw3, db3)
+        w1 -= 0.1 * dw1 / batchsize
+        b1 -= 0.1 * db1 / batchsize
+        w2 -= 0.1 * dw2 / batchsize
+        b2 -= 0.1 * db2 / batchsize
+        w3 -= 0.1 * dw3 / batchsize
+        b3 -= 0.1 * db3 / batchsize
+        if(i%10 == 0):
+            print("Series", k+1, "Epoch", i, "Loss:", loss)
 
+# Test on the test data. Prints how many it got wrong
 print("Guessing!")
 wrong = 0
 for i in range(10000):
     z1, z2, z3, a1, a2, a3 = forward(test_data[i])
     guess = np.where(a3 == np.max(a3))
     if(label1[i][guess] == 0):
-        print("Wrong!")
         wrong += 1
-print(wrong)
+print(wrong, "incorrect out of", np.shape(test_data)[0])
+print("(" + str((np.shape(test_data)[0] - wrong) / np.shape(test_data)[0] * 100) + "% success rate)")
